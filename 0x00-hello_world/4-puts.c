b724a516339f98f4a95359b4bf439034a296aab5
gechfas <gechfas@gmail.com>
2023-03-16 00:12:25 -0700

    m

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[80];
+
+	strcpy(str, "\"Programming is like building a multilingual puzzle");
+	puts(str);
+	return (0);
+}
+
